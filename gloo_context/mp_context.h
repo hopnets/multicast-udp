d9a4f8b16dc6908b6d5bcf4f82d38150296905e2
Jiacheng <Ete_Bosh@outlook.com>
2025-12-25 21:10:05 -0500

    gloo context like struct

---
+++ gloo_context/mp_context.h
@@ -0,0 +1,67 @@
+// mp_context.h
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "gloo/context.h"
+#include "gloo/rendezvous/store.h"
+
+namespace gloo {
+namespace transport {
+namespace mp {
+
+
+struct MpHandle;    // Multicast protocol handler. Should be a struct or a fd
+struct MpEndpoint;  // Connection info
+
+
+MpHandle mp_listen();
+MpHandle mp_connect(const MpEndpoint&);
+
+MpEndpoint mp_local_endpoint(const MpHandle&);
+std::string serializeEndpoint(const MpEndpoint& ep);
+MpEndpoint parseEndpoint(const std::string&);
+
+
+struct MpConnection {
+  int root = -1;
+  bool isSender = false;
+  MpHandle* handle = nullptr;
+};
+
+class MpContext {
+ public:
+  MpContext(int rank, int size);
+
+  MpContext(const MpContext&) = delete;
+  MpContext& operator=(const MpContext&) = delete;
+
+  int rank() const {
+    return rank_;
+  }
+
+  int size() const {
+    return size_;
+  }
+
+  const MpConnection& connectionForRoot(int root) const;
+  MpConnection& connectionForRoot(int root);
+
+  static std::shared_ptr<MpContext> create(
+      const std::shared_ptr<::gloo::Context>& ctx,
+      const std::shared_ptr<rendezvous::Store>& store);
+
+ private:
+  int rank_;
+  int size_;
+  std::vector<MpConnection> conns_;
+
+  void initConnectionForRoot(int root,
+                             const std::shared_ptr<rendezvous::Store>& store);
+};
+
+} // namespace mp
+} // namespace transport
+} // namespace gloo
